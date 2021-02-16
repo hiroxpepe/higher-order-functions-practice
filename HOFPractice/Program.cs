21bbcc76dc97d836391102351832e90c9cea02f0
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-02-16 22:13:46 +0900

    init

---
+++ HOFPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HOFPractice {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class Extensions {
+        public static void Each<TSource>(this IEnumerable<TSource> source, Action<TSource> action) {
+            source.ToList().ForEach(action);
+        }
+        public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector) {
+            return source.Select(selector);
+        }
+        public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
+            return source.Where(predicate);
+        }
+        public static IOrderedEnumerable<TSource> Sort<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector){
+            return source.OrderBy(keySelector);
+        }
+        public static TSource Fold<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource, TSource> func) {
+            return source.Aggregate(func);
+        }
+    }
+}
---
+++ HOFPracticeTest/ExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace HOFPractice.Tests {
+    [TestClass()]
+    public class ExtensionsTests {
+
+        [TestMethod()]
+        public void EachTest1() {
+            var _target = new[] { 3, 2, 9, 6 };
+            var _expected = new[] { 6, 4, 18, 12 };
+            var _result = new int[_target.Length];
+            var _idx = 0;
+            _target.Each(x => _result[_idx++] = x * 2);
+            CollectionAssert.AreEqual(_result, _expected);
+        }
+
+        [TestMethod()]
+        public void MapTest1() {
+            var _target = new[] { 3, 2, 9, 6 };
+            var _expected = new[] { 6, 4, 18, 12 };
+            var _result = _target.Map(x => x * 2).ToArray();
+            CollectionAssert.AreEqual(_result, _expected);
+        }
+
+        [TestMethod()]
+        public void FilterTest1() {
+            var _target = new[] { 3, 2, 9, 6 };
+            var _expected = new[] { 3, 9 };
+            var _result = _target.Where(x => x % 2 == 1).ToArray();
+            CollectionAssert.AreEqual(_result, _expected);
+        }
+
+        [TestMethod()]
+        public void SortTest1() {
+            var _target = new[] {
+                new { P = 3},
+                new { P = 2},
+                new { P = 9},
+                new { P = 6}
+            };
+            var _expected = new[] {
+                new { P = 2},
+                new { P = 3},
+                new { P = 6},
+                new { P = 9}
+            };
+            var _result = _target.Sort(x => x.P).ToArray();
+            CollectionAssert.AreEqual(_result, _expected);
+        }
+
+        [TestMethod()]
+        public void FoldTest1() {
+            var _target = new[] { 3, 2, 9, 6 };
+            var _expected = 20;
+            var _result = _target.Fold((x, y) => x + y);
+            Assert.AreEqual(_result, _expected);
+        }
+    }
+}
