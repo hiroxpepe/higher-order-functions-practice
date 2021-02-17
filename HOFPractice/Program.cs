608626978001dbd47d6e2d4438c2e62c7bc83c24
Hiroyuki Adachi <hiroxpepe@gmail.com>
2021-02-17 20:01:30 +0900

    update

--- HOFPractice/Program.cs
+++ HOFPractice/Program.cs
@@ -12,19 +12,33 @@ namespace HOFPractice {
 
     public static class Extensions {
         public static void Each<TSource>(this IEnumerable<TSource> source, Action<TSource> action) {
-            source.ToList().ForEach(action);
+            foreach (var _item in source.ToList()) {
+                action(_item);
+            }
         }
         public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector) {
-            return source.Select(selector);
+            foreach (var _item in source) {
+                yield return selector(_item);
+            }
         }
         public static IEnumerable<TSource> Filter<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) {
-            return source.Where(predicate);
+            foreach (var _item in source) {
+                if (predicate(_item)) {
+                    yield return _item;
+                }
+            }
         }
         public static IOrderedEnumerable<TSource> Sort<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector){
             return source.OrderBy(keySelector);
         }
         public static TSource Fold<TSource>(this IEnumerable<TSource> source, Func<TSource, TSource, TSource> func) {
-            return source.Aggregate(func);
+            IEnumerator<TSource> _e = source.GetEnumerator();
+            _e.MoveNext();
+            TSource _result = _e.Current;
+            while (_e.MoveNext()) {
+                _result = func(_result, _e.Current);
+            }
+            return _result;
         }
     }
 }
